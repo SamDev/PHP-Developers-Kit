f18cfe15f479c186ac4a0146778cef67eb80b636
SamDev <sam@samdev.org>
2013-05-22 14:12:51 +0200

    Update Session Constructor and Add Form Kit

--- Session/session.kit.php
+++ Session/session.kit.php
@@ -1,86 +1,143 @@
 <?php
 
-namespace DevKit;
+/**
+ * Session Manager Class 
+ * 
+ * @author SamDev
+ * 
+ * @license http://creativecommons.org/licenses/by/3.0/deed.en_US  Creative Commons Attribution 3.0
+ * 
+ * @copyright 2013 SamDev
+ * 
+ * @package PHP Developers Kit
+ * 
+ * @version 0.1
+ * 
+ */
+class Session
 {
-	class Session
-	{
-		
-		/**
-		 * Start a new Session
-		 */
-		public function __construct()
-		{
-			session_start();
-			
-		}
-			
-		public static function id()
-		{
-			return session_id();
-		}
-		
-		public static function destroy()
-		{
-			//Session Started
-			if(session_id())
-			{
-				//session_unset();
-				session_destroy();
-				//$_SESSION = array();
-			}
-		}
-		
-		protected static function started(){}
-		
-		public static function restart()
-		{
-			self::destroy();
-			
-		}
-		
-		
-		/**
-		 * Format Session Index Name to allow un-case-sensative feature
-		 * 
-		 * @param $name string the index name
-		 * 
-		 * @return string reformatted index
-		 * 
-		 * @since 0.1
-		 */
-		protected static function format($name){return strtoupper(str_replace(' ', '_', $name));}
-		
-		
-		/**
-		 * 
-		 */
-		public static function set($name, $value)
-		{
-			return session_register(self::format($name), $value);
-		}
-		
-		
-		/**
-		 * Get value of a specific session variable name
-		 * 
-		 * @
-		 */
-		public static function get($name){}
-		
-		public static function delete($name)
-		{
-			if(self::exists($name))
-			{
-				return session_unregister(self::format($name));
-			}
-			return false;
-		}
-		
-		public static function exists($name)
-		{
-			return (isset($_SESSION[self::format($name)])) ? true : false;
-		}
+	
+	/**
+	 * Class Constructor for Starting a new Session
+     * 
+     * @access public
+     * 
+     * @param $forceNew boolean if true then any previous sessions will be ended before starting a new one
+     * 
+     * @since 0.1
+	 */
+	public function __construct($forceNew = true)
+    {
+	    //Check if there is a session started before
+	    if(session_id() != '' )
+        {
+            //Force a new Id
+            if($forceNew) {session_regenerate_id(true);}
+        }
+        else
+        {
+           //No Session yet, lets start it
+           session_start();
+        }	
 	}
+    
+    /*******************************************/
+		
+	public static function id()
+    {
+		return session_id();
+    }
+    
+    /*******************************************/
+	
+	public static function destroy()
+    {
+		//Session Started
+		if(session_id())
+		{
+			
+			session_destroy();
+            $_SESSION = array();
+            var_dump(session_name());
+        }
+	}
+    
+    /*******************************************/
+	
+	/**
+     * Check whether session stated or no
+     * 
+     * @return boolean
+     * 
+     * @access protected
+     * 
+     * @since 0.1
+     */
+	protected static function started()
+    {
+	    return (session_id() == "") ? false : true;
+	}
+	
+    /*******************************************/
+    
+	public static function restart()
+    {
+		self::destroy();
+        
+    }
+	
+    /*******************************************/
+	
+	/**
+	 * Format Session Index Name to allow un-case-sensative feature
+	 * 
+	 * @param $name string the index name
+	 * 
+	 * @return string reformatted index
+     * 
+     * @access protected
+	 * 
+	 * @since 0.1
+	 */
+	protected static function format($name){return strtoupper(str_replace(' ', '_', $name));}
+    
+    /*******************************************/
+	
+	/**
+	 * 
+	 */
+	public static function set($name, $value)
+	{
+		return session_register(self::format($name), $value);
+	}
+	
+    /*******************************************/
+	
+	/**
+	 * Get value of a specific session variable name
+	 * 
+	 * @
+	 */
+	public static function get($name){}
+	
+	public static function delete($name)
+	{
+		if(self::exists($name))
+		{
+			return session_unregister(self::format($name));
+		}
+		return false;
+	}
+    
+    /*******************************************/
+	
+	public static function exists($name)
+	{
+		return (isset($_SESSION[self::format($name)])) ? true : false;
+	}
+    
+    /*******************************************/
 }
+
 
 
